a161e34e08928645c09ee83105dfc9ee82d8b91a
Nihara Rajesh <nihraj297@gmail.com>
2026-08-25 00:46:36 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,32 @@
+class MinStack {
+    stack<int>s;
+    stack<int>ms;
+public:
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s.push(val);
+        if(ms.empty()){
+            ms.push(val);
+        }
+        else{
+            ms.push(min(val, ms.top()));
+        }
+    }
+    
+    void pop() {
+        s.pop();
+        ms.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return ms.top();
+    }
+};
